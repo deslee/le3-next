d9dc17564323ef4ecc270cd76d2587dd48cd536f
Desmond Lee <desmondclee@gmail.com>
2018-08-18 17:19:47 -0500

    new vbranch

--- pages/index.js
+++
@@ -1,6 +0,0 @@
-import Layout from '../components/Layout'
-import Resume from '../components/resume/Resume';
-
-export default () => <Layout title="Desmond Lee">
-    <Resume />
-</Layout>
---
+++ pages/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Landing = styled.div`
+    font-family: -apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", "Fira Sans", Avenir, "Helvetica Neue", "Lucida Grande", sans-serif;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+const Title = styled.h1`
+    font-weight: 100;
+`
+
+
+export default () => <Landing>
+        <Title>My page</Title>
+</Landing>
